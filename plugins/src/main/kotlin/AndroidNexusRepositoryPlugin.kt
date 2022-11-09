47cc0d62fcec60f8cd12b5bc80b209ae49cd64eb
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2022-11-09 16:38:09 +0100

    Change repository to MavenCentral

--- plugins/src/main/kotlin/AndroidNexusRepositoryPlugin.kt
+++ plugins/src/main/kotlin/AndroidNexusRepositoryPlugin.kt
@@ -55,7 +55,7 @@ class AndroidNexusRepositoryPlugin : Plugin<Project> {
         }
 
         publishing.repositories {
-            maven {
+            mavenCentral {
                 credentials {
                     username = System.getenv("OSSR_USERNAME")
                     password = System.getenv("OSSR_PASSWORD")
