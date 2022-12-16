32c9363e740b47061f48f90085488cd5373b9c60
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-12-16 15:40:02 +0100

    Nexus publication script improvements

--- plugins/src/main/kotlin/AndroidNexusRepositoryPlugin.kt
+++ plugins/src/main/kotlin/AndroidNexusRepositoryPlugin.kt
@@ -34,9 +34,8 @@ import no.nordicsemi.android.buildlogic.getVersionNameFromTags
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPom
 import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.tasks.bundling.AbstractArchiveTask
-import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.extra
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByType
@@ -52,134 +51,130 @@ class AndroidNexusRepositoryPlugin : Plugin<Project> {
                 apply("signing")
             }
 
-            val nexusPluginExt: NexusRepositoryPluginExt = extensions.create("nordicNexusPublishing", NexusRepositoryPluginExt::class.java)
-            val library = extensions.getByType<LibraryExtension>()
+            // Default Nordic group.
+            group = "no.nordicsemi.android"
 
+            val nexusPluginExt = extensions.create("nordicNexusPublishing", NexusRepositoryPluginExt::class.java)
+            val library = extensions.getByType<LibraryExtension>()
+            val signing = extensions.getByType<SigningExtension>()
+
+            // The signing configuration will be user by signing plugin.
             extra.set("signing.keyId", System.getenv("GPG_SIGNING_KEY"))
             extra.set("signing.password", System.getenv("GPG_PASSWORD"))
-            extra.set("signing.secretKeyRingFile", "../sec.gpg")
+            extra.set("signing.secretKeyRingFile", "${project.rootDir.path}/sec.gpg")
 
+            // Create a software component with the release variant.
             library.publishing {
-                singleVariant("sources") {
+                singleVariant("release") {
                     withSourcesJar()
                 }
             }
 
-            project.afterEvaluate {
-                project.tasks.create("androidSourcesJar", Jar::class.java) {
-                    afterEvaluate {
-                        archiveClassifier.set("sources")
-                        from(library.sourceSets.getByName("main").java.srcDirs)
+            afterEvaluate {
+                publishing {
+                    repositories {
+                        maven {
+                            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                            credentials {
+                                username = System.getenv("OSSR_USERNAME")
+                                password = System.getenv("OSSR_PASSWORD")
+                            }
+                        }
+                    }
+                    publications {
+                        val publication = create("mavenPublication", MavenPublication::class.java) {
+                            // Set publication properties.
+                            with (nexusPluginExt) {
+                                artifactId = POM_ARTIFACT_ID
+                                version = getVersionNameFromTags()
+                                groupId = GROUP ?: group.toString()
+                            }
+                            // Set the component to be published.
+                            from(components["release"])
+                            // Apply POM configuration.
+                            pom {
+                                from(nexusPluginExt)
+                            }
+                        }
+                        // This task will add *.asc files to the publication for all artifacts.
+                        signing.sign(publication)
                     }
                 }
-
-                project.configurePublishingExtension(nexusPluginExt)
             }
         }
     }
 
-    private fun Project.configurePublishingExtension(nexusPluginExt: NexusRepositoryPluginExt) {
-        val publishing = extensions.getByType(PublishingExtension::class.java)
-        val signing = extensions.getByType<SigningExtension>()
-
-        val androidSourcesJar = tasks.getByName("androidSourcesJar") as AbstractArchiveTask
-
-        artifacts {
-            add("archives", androidSourcesJar)
-        }
-
-        publishing.repositories {
-            maven {
-                setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-                credentials {
-                    username = System.getenv("OSSR_USERNAME")
-                    password = System.getenv("OSSR_PASSWORD")
-                }
-            }
-        }
-        publishing.publications {
-            this.register("mavenPublication", MavenPublication::class.java) {
-                from(components["release"])
-                if (!project.state.executed) {
-                    project.afterEvaluate {
-                        configureDescription(this@register, nexusPluginExt, androidSourcesJar)
-                    }
-                } else {
-                    configureDescription(this@register, nexusPluginExt, androidSourcesJar)
-                }
-            }
-        }
-
-        signing.sign(publishing.publications.getByName("mavenPublication"))
+    private fun Project.publishing(configuration: PublishingExtension.() -> Unit) {
+        val publishing = extensions.getByType<PublishingExtension>()
+        configuration(publishing)
     }
 
-    private fun Project.configureDescription(
-        publication: MavenPublication,
+    private fun MavenPom.from(
         nexusPluginExt: NexusRepositoryPluginExt,
-        androidSourcesJar: AbstractArchiveTask
-    ) {
-        publication.artifactId = nexusPluginExt.POM_ARTIFACT_ID
-        publication.groupId = nexusPluginExt.GROUP
-        publication.version = getVersionNameFromTags()
+    ) = with (nexusPluginExt) {
+        name.set(POM_NAME)
+        description.set(POM_DESCRIPTION)
+        url.set(POM_URL)
+        packaging = POM_PACKAGING
 
-        publication.artifact(androidSourcesJar)
-
-        publication.pom {
-            name.set(nexusPluginExt.POM_NAME)
-            packaging = nexusPluginExt.POM_PACKAGING
-            description.set(nexusPluginExt.POM_DESCRIPTION)
-            url.set(nexusPluginExt.POM_URL)
-
-            licenses {
-                license {
-                    name.set(nexusPluginExt.POM_LICENCE)
-                    url.set(nexusPluginExt.POM_LICENCE_URL)
-                    distribution.set("repo")
-                }
+        // https://maven.apache.org/pom.html#licenses
+        licenses {
+            license {
+                name.set(POM_LICENCE)
+                url.set(POM_LICENCE_URL)
+                // The two stated methods are repo (they may be downloaded from a Maven repository) or manual (they must be manually installed).
+                distribution.set("repo")
             }
-            
-            scm {
-                url.set(nexusPluginExt.POM_SCM_URL)
-                connection.set(nexusPluginExt.POM_SCM_CONNECTION)
-                developerConnection.set(nexusPluginExt.POM_SCM_DEV_CONNECTION)
-            }
+        }
 
-            organization {
-                name.set(nexusPluginExt.POM_ORG)
-                url.set(nexusPluginExt.POM_ORG_URL)
-            }
+        // https://maven.apache.org/pom.html#scm
+        scm {
+            url.set(POM_SCM_URL)
+            connection.set(POM_SCM_CONNECTION)
+            developerConnection.set(POM_SCM_DEV_CONNECTION)
+        }
 
-            developers {
-                developer {
-                    id.set(nexusPluginExt.POM_DEVELOPER_ID)
-                    name.set(nexusPluginExt.POM_DEVELOPER_NAME)
-                    email.set(nexusPluginExt.POM_DEVELOPER_EMAIL)
-                    organization.set(nexusPluginExt.POM_ORG)
-                    organizationUrl.set(nexusPluginExt.POM_ORG_URL)
-                }
+        // https://maven.apache.org/pom.html#organization
+        organization {
+            name.set(POM_ORG)
+            url.set(POM_ORG_URL)
+        }
+
+        // https://maven.apache.org/pom.html#developers
+        developers {
+            developer {
+                id.set(POM_DEVELOPER_ID)
+                name.set(POM_DEVELOPER_NAME)
+                email.set(POM_DEVELOPER_EMAIL)
+                organization.set(POM_ORG)
+                organizationUrl.set(POM_ORG_URL)
             }
         }
     }
 }
 
-abstract class NexusRepositoryPluginExt(
-    var POM_ARTIFACT_ID: String = "",
-    var POM_NAME: String = "",
-    var POM_PACKAGING: String = "aar",
+abstract class NexusRepositoryPluginExt {
+    // Required parameters:
+    lateinit var POM_ARTIFACT_ID: String
+    lateinit var POM_NAME: String
+    lateinit var POM_DESCRIPTION: String
+    lateinit var POM_URL: String
+    lateinit var POM_SCM_URL: String
+    lateinit var POM_SCM_CONNECTION: String
+    lateinit var POM_SCM_DEV_CONNECTION: String
 
-    var GROUP: String = "no.nordicsemi.android",
-    var POM_DESCRIPTION: String = "Nordic Android Common Libraries",
-    var POM_URL: String = "https://github.com/NordicSemiconductor/Android-Gradle-Plugins",
-    var POM_SCM_URL: String = "https://github.com/NordicSemiconductor/Android-Gradle-Plugins",
-    var POM_SCM_CONNECTION: String = "scm:git@github.com:NordicSemiconductor/Android-Gradle-Plugins.git",
-    var POM_SCM_DEV_CONNECTION: String = "scm:git@github.com:NordicSemiconductor/Android-Gradle-Plugins.git",
+    // Default values:
+    var POM_PACKAGING: String = "aar"
+    var GROUP: String? = null // If not set, the project's "group" will be used.
+
     // License
-    var POM_LICENCE: String = "BSD-3-Clause",
-    var POM_LICENCE_URL: String = "http://opensource.org/licenses/BSD-3-Clause",
+    var POM_LICENCE: String = "BSD-3-Clause"
+    var POM_LICENCE_URL: String = "http://opensource.org/licenses/BSD-3-Clause"
+
     // Developer
-    var POM_DEVELOPER_ID: String = "mag",
-    var POM_DEVELOPER_NAME: String = "Mobile Applications Group",
-    var POM_DEVELOPER_EMAIL: String = "mag@nordicsemi.no",
-    var POM_ORG: String = "Nordic Semiconductor ASA",
+    var POM_DEVELOPER_ID: String = "mag"
+    var POM_DEVELOPER_NAME: String = "Mobile Applications Group"
+    var POM_DEVELOPER_EMAIL: String = "mag@nordicsemi.no"
+    var POM_ORG: String = "Nordic Semiconductor ASA"
     var POM_ORG_URL: String = "https://www.nordicsemi.com"
-)
+}
