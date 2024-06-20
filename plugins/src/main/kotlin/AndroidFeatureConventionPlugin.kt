3c50b39b2d1b89a2f29aee573782ceee7ebfdc6e
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2024-06-20 23:29:00 +0200

    Imports organized

--- plugins/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ plugins/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -32,9 +32,7 @@
 import no.nordicsemi.android.buildlogic.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 
 class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
