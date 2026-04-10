23686dd78933c608058cd0b027f83f86cb2e12ca
Roman Makeev <makeevrserg@gmail.com>
2026-04-10 23:31:01 +0300

    remove unused android target checks
    (cherry picked from commit e24046252c341ceb6d2005322cbdf41ac260d712)

--- plugins/src/main/kotlin/no/nordicsemi/ProjectExt.kt
+++ plugins/src/main/kotlin/no/nordicsemi/ProjectExt.kt
@@ -32,21 +32,8 @@
 package no.nordicsemi
 
 import org.gradle.api.JavaVersion
-import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-
-internal val Project.hasAndroidKmpPlugin: Boolean
-    get() = plugins.hasPlugin("com.android.kotlin.multiplatform.library")
-
-internal val Project.hasAndroidLibPlugin: Boolean
-    get() = plugins.hasPlugin("com.android.library")
-
-internal val Project.hasAndroidAppPlugin: Boolean
-    get() = plugins.hasPlugin("com.android.application")
-
-internal val Project.hasAndroidPlugin: Boolean
-    get() = hasAndroidAppPlugin || hasAndroidLibPlugin
 
 internal fun JavaVersion.asJvmTarget(): JvmTarget {
     return JvmTarget.fromTarget(majorVersion)
